fd18eddfc72e4f6562e83feb268497032da8b8be
Максим Егоров <66911064+egorov-ma@users.noreply.github.com>
2025-08-07 19:59:20 +0400

    refactor: use AuthService for auth

--- frontend/config/vite/dev.config.ts
+++ frontend/config/vite/dev.config.ts
@@ -18,6 +18,12 @@ export default ({ mode }) => {
         usePolling: true,
         interval: 1000,
       },
+      proxy: {
+        '/api': {
+          target: 'http://localhost:8080',
+          changeOrigin: true,
+        },
+      },
     },
     test: {
       globals: true,
