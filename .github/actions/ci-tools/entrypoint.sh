3be64df991606a69ebeac6b5848202afa06cb633
egorovma <link@egorovma.ru>
2025-09-23 07:12:31 +0400

    ci: replace workflows and remove custom actions

--- .github/actions/ci-tools/entrypoint.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-set -e
-mkdir -p /github/workspace/.ci-tools
-cp /usr/local/bin/yq /github/workspace/.ci-tools/
-cp /usr/local/bin/shellcheck /github/workspace/.ci-tools/
-cp /usr/local/bin/hadolint /github/workspace/.ci-tools/
-chmod +x /github/workspace/.ci-tools/*
-echo "/github/workspace/.ci-tools" >> "$GITHUB_PATH"
