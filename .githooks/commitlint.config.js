998b505bdae6c6f207f46e8cd9024be2eb8333fb
egorovma <link@egorovma.ru>
2025-09-25 02:40:09 +0400

    ci: commitlint

--- .githooks/commitlint.config.js
+++ .githooks/commitlint.config.js
@@ -1,6 +1,9 @@
 module.exports = {
   extends: ['@commitlint/config-conventional'],
   rules: {
-    'type-enum': [2, 'always', ['build','chore','ci','docs','feat','fix','perf','refactor','revert','style','test']],
+    // Allow an extended set of types used in this repo
+    'type-enum': [2, 'always', ['build','chore','ci','docs','feat','fix','perf','refactor','revert','style','test','config','infra','deps']],
+    // Do not enforce subject case to allow titles with proper nouns (e.g., "CodeQL", "Docker Images CI")
+    'subject-case': [0],
   },
 };
