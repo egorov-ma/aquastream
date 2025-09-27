3777c43d0442d2f43bfbcbbc24f79cf01680970d
egorovma <link@egorovma.ru>
2025-09-28 03:31:45 +0400

    perf: frontend refactoring task#10

--- frontend/.eslintrc.js
+++ frontend/.eslintrc.js
@@ -1,3 +1,19 @@
 module.exports = {
   extends: ["next/core-web-vitals", "next/typescript"],
+  overrides: [
+    {
+      files: ["app/**/*.{ts,tsx}", "components/**/*.{ts,tsx}"],
+      rules: {
+        "no-restricted-syntax": [
+          "error",
+          {
+            selector:
+              "JSXAttribute[name.name='className'][value.type='Literal'][value.value=/#/]",
+            message:
+              "Запрещены raw-классы с hex-цветами. Используйте дизайн-токены или переменные (например, var(--color)).",
+          },
+        ],
+      },
+    },
+  ],
 };
